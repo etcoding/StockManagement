fec63c616a8d08bf1400dc4c882f84b4bb594334
Evgeni Tsarovski <evgeni@etcoding.com>
2012-11-18 16:58:47 +1300

    Initial commit

--- StockManagement/Controllers/StoreController.cs
+++ StockManagement/Controllers/StoreController.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using StockManagement.Attributes;
 using StockManagement.Models;
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,84 +15,33 @@ namespace StockManagement.Controllers
         [Queryable(ResultLimit = 200)]
         public IQueryable<Store> GetAll()
         {
-            using (var session = Raven.Instance.Store.OpenSession())
-            {
-                var categories = session.Query<Store>();
-                return categories;
-            }
+            throw new NotImplementedException();
         }
 
         // GET api/Store/5
         public Store Get(string id)
         {
-            using (var session = Raven.Instance.Store.OpenSession())
-            {
-                var Store = session.Query<Store>().SingleOrDefault(x => x.Id == id);
-                if (Store == null)
-                    throw new HttpResponseException(HttpStatusCode.NotFound);
-                return Store;
-            }
+            throw new NotImplementedException();
         }
 
         // POST api/Store
         [HttpPost]
         public HttpResponseMessage CreateStore([FromBody]Store Store)
         {
-            if (!string.IsNullOrEmpty(Store.Id))
-                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.BadRequest) { ReasonPhrase = "Why is id set?" });
-
-            if (!ModelState.IsValid)
-                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    ReasonPhrase = "Data is invalid",
-                    Content = new StringContent(JsonConvert.SerializeObject(ModelState))
-                });
-
-            using (var session = Raven.Instance.Store.OpenSession())
-            {
-                session.Store(Store);
-                session.SaveChanges();
-                var response = this.Request.CreateResponse<Store>(HttpStatusCode.Created, Store);
-
-                return response;
-            }
+            throw new NotImplementedException();
         }
 
         // PUT api/Store/5
         [HttpPut]
         public HttpStatusCode Update(string id, [FromBody]Store Store)
         {
-            if (!ModelState.IsValid)
-                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    ReasonPhrase = "Data is invalid",
-                    Content = new StringContent(JsonConvert.SerializeObject(ModelState))
-                });
-
-            using (var session = Raven.Instance.Store.OpenSession())
-            {
-                var cat = session.Load<Store>(id);
-                if (cat == null)
-                    return HttpStatusCode.NotFound;
-
-                cat.Name = Store.Name;
-                session.SaveChanges();
-                return HttpStatusCode.OK;
-            }
+            throw new NotImplementedException();
         }
 
         // DELETE api/Store/5
         public HttpStatusCode Delete(int id)
         {
-            using (var session = Raven.Instance.Store.OpenSession())
-            {
-                var cat = session.Load<Store>(id);
-                if (cat == null)
-                    return HttpStatusCode.NotFound;
-
-                session.Delete<Store>(cat);
-                return HttpStatusCode.OK;
-            }
+            throw new NotImplementedException();
         }
     }
 }
